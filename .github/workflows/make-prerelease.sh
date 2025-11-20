cabf74b044b813a3f1c83981c5454dcc9116effb
Jeffrey Meerovici Goryn <j.g.meerovicigoryn@student.tudelft.nl>
2025-11-20 15:55:03 +0100

    Changing prerelease to work with timestamps as mentioned in the assignment as a suggestion

--- .github/workflows/make-prerelease.sh
+++ .github/workflows/make-prerelease.sh
@@ -3,28 +3,22 @@ set -e
 
 BRANCH="${BRANCH_NAME:-unknown}"
 
-if [[ "$BRANCH" == "main" || "$BRANCH" == "master" ]]; then
+if [["$BRANCH" == "master" ]]; then
   echo "Not creating prerelease on main branch."
   exit 0
 fi
 
-# Extract base version (e.g., 1.2.0)
+# Extract base version and removes snapshot
 VERSION=$(xmlstarlet sel -t -v "/project/version" pom.xml)
 BASE_VERSION=$(echo "$VERSION" | sed 's/-SNAPSHOT//')
 
-# Example prerelease tag: v1.2.0-alpha.1
+
 LABEL=$(echo "$BRANCH" | tr '/' '-' )
 PREFIX="v${BASE_VERSION}-${LABEL}"
 
-# Find latest prerelease of this branch
-LAST=$(git tag --list "${PREFIX}.*" | sort -V | tail -1)
+TIMESTAMP=$(date +%d%m%y-%H%M%S)
 
-if [[ -z "$LAST" ]]; then
-  NEXT="${PREFIX}.1"
-else
-  NUM=$(echo "$LAST" | awk -F. '{print $NF}')
-  NEXT="${PREFIX}.$((NUM + 1))"
-fi
+NEXT="v${BASE_VERSION}-${LABEL}-${TIMESTAMP}"
 
 echo "Creating prerelease tag: $NEXT"
 
